be1e213c6a1b2a14e7613adfab2173822e13ea13
Hururuk <hururuk@kookmin.ac.kr>
2020-06-20 19:20:35 +0900

    issue #6 "Modify hw1.py"

--- calc/hw1.py
+++ calc/hw1.py
@@ -4,19 +4,16 @@ from template import html
 def application(environ, start_response) :
     d = parse_qs(environ['QUERY_STRING'])
     
-    a = d.get('a', [''])[0]
-    b = d.get('b', [''])[0]
-    if '' not in [a, b] :
-        a, b = int(a), int(b)
-    x = a + b
-    y = a * b
-    response_body = html % {
-            'x' : x,
-            'y' : y
-            }
+    first_num = d.get('first_num', [''])[0]
+    second_num = d.get('second_num', [''])[0]
+    sum, mul = 0, 0
+    if '' not in [first_num, second_num] :
+        first_num, second_num = int(first_num), int(second_num)
+        sum = first_num + second_num
+        mul = first_num * second_num
+    response_body = html % {'sum':sum, 'mul':mul}
 
     status = '200 OK'
     response_headers = [('Content-Type', 'text/html'), ('Content-Length', str(len(response_body)))]
 
-    start_response(status, response_headers)
     return [response_body]
