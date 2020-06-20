eb8c8dab7ab300db39959c7439a584999432aab9
Hururuk <hururuk@kookmin.ac.kr>
2020-06-20 20:40:31 +0900

    issue #11 "Modify hw1.py"

--- calc/hw1.py
+++ calc/hw1.py
@@ -18,5 +18,6 @@ def application(environ, start_response) :
 
     status = '200 OK'
     response_headers = [('Content-Type', 'text/html'), ('Content-Length', str(len(response_body)))]
-
+    
+    start_response(status, response_headers)
     return [response_body]
